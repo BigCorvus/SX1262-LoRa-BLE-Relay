4c1ba8ad3aa4f097b543de8df76320766061ec97
Arthur Jordan <arthur_jordan05@web.de>
2020-06-11 13:35:10 +0200

    Update LORA_RELAY_NRF52840.ino

--- LORA_RELAY_NRF52840.ino
+++ LORA_RELAY_NRF52840.ino
@@ -1,26 +1,25 @@
 /*********************************************************************
-  This is an example for our nRF52 based Bluefruit LE modules
+  This is a basic firmware for for the LORA BLE relays
+  Based on the BLE UART example by Adafruit. 
+  Tested on https://github.com/adafruit/Adafruit_nRF52_Arduino core 0.20.1 
+  Demontrates basic RX TX functionality by forwarding BLE data to LORA and vice versa, so the nRF UART app can be used to have a conversation via LORA.
+  Also implements all the housekeeping such as the BQ27441 fuel gauge, ST7735 LCD and buttons.
+  If using the Feather nrf52840 express bootloader the SWITCH button can be used to enter bootloader mode if pressed on startup. 
+  Note that you need to modify the variant.h and variant.cpp as described below in order to get software acces to certain pins. See the core documentation for more info on where to find those files. 
+  Also note that the PHOLD pin was separated from the rest of the circuit by cutting a trace on the PCB between the P0.05 pad on the nrf52840 module and the via next to it so the power latching circuit is not used.  
+  This will be corrected in an upcoming version of the design files. 
 
-  Pick one up today in the adafruit shop!
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  MIT license, check LICENSE for more information
-  All text above, and the splash screen below must be included in
-  any redistribution
 *********************************************************************/
 #include <bluefruit.h>
-//#include <Adafruit_LittleFS.h>
-//#include <InternalFileSystem.h>
+#include <Adafruit_LittleFS.h>
+#include <InternalFileSystem.h>
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 #include <SPI.h>
 #include <Wire.h>
 #include <RadioLib.h>
 #include <SparkFunBQ27441.h>
-#include "Arduino.h"
+//#include "Arduino.h"
 
 // Set BATTERY_CAPACITY to the design capacity of your battery.
 const unsigned int BATTERY_CAPACITY = 700; // e.g. 850mAh battery
@@ -28,25 +27,33 @@ const unsigned int BATTERY_CAPACITY = 700; // e.g. 850mAh battery
 
 unsigned int soc = 0;
 /*
-   in variant.cpp:
+   the following changes need to be made:
+   in variant.cpp uncomment those pins:
   // The remaining pins are not connected (per schematic)
-   33,      //P1.01 is not connected per schematic
-   35,      //P1.03 is not connected per schematic
-   36,      // P1.04 is not connected per schematic
-   37,      // P1.05 is not connected per schematic
-   38,      // P1.06 is not connected per schematic
-   39,      // P1.07 is not connected per schematic
-   43,      // P1.11 is not connected per schematic
-   44,      //P1.12 is not connected per schematic
-   45,      // P1.13 is not connected per schematic
-   46,      //P1.14 is not connected per schematic
+  // The following pins were never listed as they were considered unusable
+    0,      // P0.00 is XL1   (attached to 32.768kHz crystal)
+    1,      // P0.01 is XL2   (attached to 32.768kHz crystal)
+   18,      // P0.18 is RESET (attached to switch)
+   32,      // P1.00 is SWO   (attached to debug header)
+
+  // The remaining pins are not connected (per schematic)
+   33,      //D38 is P1.01 is not connected per schematic
+   35,      //D39 is P1.03 is not connected per schematic
+   36,      //D40 P1.04 is not connected per schematic
+   37,      //D41 P1.05 is not connected per schematic
+   38,      //D42 P1.06 is not connected per schematic
+   39,      //D43 P1.07 is not connected per schematic
+   43,      //D44 P1.11 is not connected per schematic
+   44,      //D45 is P1.12 is not connected per schematic
+   45,      //D46 P1.13 is not connected per schematic
+   46,      //D47 is P1.14 is not connected per schematic
 
    and in variant.h:
    #define PINS_COUNT           (48)
   #define NUM_DIGITAL_PINS     (48)
 
 */
-
+//the pin definitons are quite confusing, so watch out
 #define DIO1 (10)//P0.27
 #define DIO2 (17//P0.28
 #define TXEN (16)//P0.30
@@ -60,7 +67,7 @@ unsigned int soc = 0;
 #define BTN_UP (38)//P1.01 -->33? actually 38
 #define SWITCH (7)//P1.02
 
-#define LED2 (44)//P1.11!
+#define LED2 (44)//P1.11! actually wanted to connect it to 1.10 but made a mistake on the pcb, now it's on 1.11.
 #define LED1 (4)//P1.15
 #define PIEZO (47)//P1.14 -->46? actually 47
 
@@ -91,13 +98,14 @@ volatile bool receivedFlag = false;
 volatile bool enableInterrupt = true;
 
 // For 1.44" and 1.8" TFT with ST7735 use
-Adafruit_ST7735 tft = Adafruit_ST7735(D_CS,  D_RS, D_RES); 
+Adafruit_ST7735 tft = Adafruit_ST7735(D_CS,  D_RS, D_RES);
 
 void setup()
 {
   //WTF, Serial and the fuel gauge has to be initialized prior to the pins....
+  //delay(1000);
   Serial.begin(115200);
-  delay(2000);
+  delay(5000);
   setupBQ27441();
   soc = lipo.soc();  // Read state-of-charge (%)
 
@@ -118,7 +126,7 @@ void setup()
   digitalWrite(L_RST, LOW);
   delay(100);
   digitalWrite(L_RST, HIGH);
-  prepareRX();
+  prepareRX(); //switch RXEN and TXEN
   //Wire.begin();
 
 
@@ -270,7 +278,7 @@ void startAdv(void)
   //printBatteryStats();
 }
 
-  
+
 void setupBQ27441(void)
 {
   // Use lipo.begin() to initialize the BQ27441-G1A and confirm that it's
@@ -327,15 +335,63 @@ void loop()
     int count = Serial.readBytes(buf, sizeof(buf));
     bleuart.write( buf, count );
   }
+  if (bleuart.available() > 0) {
+    int BLEbytes = 0;
+    uint8_t BLEbuf[64];
+    // Forward from BLEUART to HW Serial
+    while ( bleuart.available() )
+    {
+      uint8_t ch;
+      ch = (uint8_t) bleuart.read();
+      Serial.write(ch);
+      BLEbuf[BLEbytes] = ch;
+      BLEbytes++;
+    }
 
-  // Forward from BLEUART to HW Serial
-  while ( bleuart.available() )
-  {
-    uint8_t ch;
-    ch = (uint8_t) bleuart.read();
-    Serial.write(ch);
+    //now transmit via LORA
+    prepareTX();
+    Serial.print(F("[SX1262] Transmitting packet ... "));
+
+    // you can transmit C-string or Arduino string up to
+    // 256 characters long
+    // NOTE: transmit() is a blocking method!
+    //       See example SX126x_Transmit_Interrupt for details
+    //       on non-blocking transmission method.
+    //int state = lora.transmit("Hello World!");
+
+    // you can also transmit byte array up to 256 bytes long
+    
+      
+      int state = lora.transmit(BLEbuf, BLEbytes+1);
+    
+
+    if (state == ERR_NONE) {
+      // the packet was successfully transmitted
+      Serial.println(F("success!"));
+
+      // print measured data rate
+      Serial.print(F("[SX1262] Datarate:\t"));
+      Serial.print(lora.getDataRate());
+      Serial.println(F(" bps"));
+      delay(200);
+      prepareRX();
+
+    } else if (state == ERR_PACKET_TOO_LONG) {
+      // the supplied packet was longer than 256 bytes
+      Serial.println(F("too long!"));
+
+    } else if (state == ERR_TX_TIMEOUT) {
+      // timeout occured while transmitting packet
+      Serial.println(F("timeout!"));
+
+    } else {
+      // some other error occurred
+      Serial.print(F("failed, code "));
+      Serial.println(state);
+
+    }
+
   }
-
   // check if the flag is set
   if (receivedFlag) {
     // disable the interrupt service routine while
@@ -348,6 +404,7 @@ void loop()
     // you can read received data as an Arduino String
     String str;
     int state = lora.readData(str);
+
 
     // you can also read received data as byte array
     /*
@@ -362,7 +419,7 @@ void loop()
       // print data of the packet
       Serial.print(F("[SX1262] Data:\t\t"));
       Serial.println(str);
-
+      bleuart.print(str); //send via BLE
       // print RSSI (Received Signal Strength Indicator)
       Serial.print(F("[SX1262] RSSI:\t\t"));
       Serial.print(lora.getRSSI());
@@ -399,7 +456,7 @@ void loop()
 void up_callback(void)
 {
   digitalWrite(LED2, HIGH);
-  
+  //lora.sleep();
 }
 
 void sw_callback(void)
@@ -442,6 +499,7 @@ void ok_callback(void)
     Serial.print(F("[SX1262] Datarate:\t"));
     Serial.print(lora.getDataRate());
     Serial.println(F(" bps"));
+    delay(200);
     prepareRX();
 
   } else if (state == ERR_PACKET_TOO_LONG) {
@@ -460,12 +518,12 @@ void ok_callback(void)
   }
 }
 
-void prepareTX(void){
+void prepareTX(void) {
   digitalWrite(TXEN, HIGH);
   digitalWrite(RXEN, LOW);
 }
 
-void prepareRX(void){
+void prepareRX(void) {
   digitalWrite(TXEN, LOW);
   digitalWrite(RXEN, HIGH);
 }
